bea8e0d27dbb28a537b41f96110cb555dda68366
cortezd334 <cortezd334@gmail.com>
2020-12-06 22:11:16 -0800

    deploy react app;

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Danny's App</h1>
+      <h2>Test</h2>
+    </div>
+  );
+}
+
+export default App;
