8b92167a1af34060d7a14c14720a7a45c5dc3873
cortezd334 <cortezd334@gmail.com>
2020-12-07 02:27:42 -0800

    final back in public

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,9 +10,9 @@ export default function Contact() {
         <div className='contact'>
             <h1>'I'm contact'</h1>
             <div className='icons'>
-                <a href='https://www.linkedin.com/in/daniracortez/' target='_blank'><img src={linkedin} alt='Linkedin'/></a><br/>
-                <a href='https://github.com/cortezd334' target='_blank'><img src={github} alt='Github'/></a><br/>
-                <a href='https://medium.com/@cortezd334' target='_blank'><img src={medium} alt='Medium'/></a><br/>
+                <a href='https://www.linkedin.com/in/daniracortez/' rel="noreferrer" target='_blank'><img src={linkedin} alt='Linkedin'/></a><br/>
+                <a href='https://github.com/cortezd334' rel="noreferrer" target='_blank'><img src={github} alt='Github'/></a><br/>
+                <a href='https://medium.com/@cortezd334' rel="noreferrer" target='_blank'><img src={medium} alt='Medium'/></a><br/>
                 <a href={'mailto:cortezd334@gmail.com'}><img src={paper_plane} alt='Email'/></a>
             </div>
         </div>
