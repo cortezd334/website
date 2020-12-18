b6ef632b7badadf01f9c8c5e541acf332d7d5a92
cortezd334 <cortezd334@gmail.com>
2020-12-17 18:04:44 -0800

    cards

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-
+import ToText from '../utils/ToText'
 export default function Blog({blog, profile}) {
 
     console.log(blog)
@@ -10,27 +10,24 @@ export default function Blog({blog, profile}) {
         if(blog.item) {
             return blog.item.map((post, index) => (
                 <> 
+                <div className={'col-md-4 col-sm-6 col-xs-12 grid'}>
                     <div className='card' key={index}>
-                        <div className='card-image' style={{backgroundImage: 'url(${post.thumbnail})'}}>
+                        <div className='card-image' style={{backgroundImage: `url(${post.thumbnail})`}}>
                             <div className='authorimg'>
-                                <a href={profile.profileurl} rel='noopener noreferrer' target='_blank' style={{backgroundImage: 'url(${profile.avtar})'}}>
+                                <a href={profile.profileurl} rel='noopener noreferrer' target='_blank' style={{backgroundImage: `url(${profile.avtar})`}}>
                                     Written By Danira
                                 </a>
                             </div>
                         </div>
                         <div className='card-body'>
                             <h5 className='card-title'>
-                                <Link to={post.title}>{post.title}</Link>
+                                <Link to={post.title} className='textfiordblue'>{post.title}</Link>
                             </h5>
-                            <p>{`${String(post.description.substring(0, 1000))}...`}</p>
+                            <p className='cardText'>{`${ToText(post.description.substring(0, 1000))}...`}</p>
                             <br/>
-                            {post.author}
-                            <br/>
-                            <span className='text-muted'>
-                                {/* {moment(post.pubDate).format('MMM DD, YYYY hh:mm')} */}
-                            </span>
                         </div>
                     </div>
+                </div>
                 </>
             ))
         }
@@ -38,8 +35,10 @@ export default function Blog({blog, profile}) {
     return(
         <>
             <h2>Blog Posts</h2>
-            <div className={'col-md-4 col-sm-6 col-xs-12 grid'}>
-                {blog.isLoading? 'nothing yet' : haveBlogs()}
+            <div className='container'>
+                {/* <div className='row'> */}
+                    {blog.isLoading? 'nothing yet' : haveBlogs()}
+                {/* </div> */}
             </div>
         </>
     )
