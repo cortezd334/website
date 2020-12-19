947b80ee13929647ce41a39e33e262fe9f54b298
cortezd334 <cortezd334@gmail.com>
2020-12-18 22:32:50 -0800

    email location

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,12 +8,19 @@ export default function Contact() {
     
     return(
         <div className='contact'>
-            <h2></h2>
+            <h2>Contact Me!</h2>
+            <p>Please feel free to reach out regarding any career opportunities, networking, or if you have any questions.</p>
+            <h3>Email</h3>
+            <p>cortedz334@gmail.com</p>
+            <h3>Location</h3>
+            <p>Seattle</p>
+            <br/>
+            <h3>Find me on...</h3>
             <div className='icons'>
+                <a href={'mailto:cortezd334@gmail.com'} className='email'><img src={paper_plane} alt='Email'/></a>
                 <a href='https://www.linkedin.com/in/daniracortez/' rel="noreferrer" target='_blank' className='icon'><img src={linkedin} alt='Linkedin'/></a><br/>
                 <a href='https://github.com/cortezd334' rel="noreferrer" target='_blank' className='icon'><img src={github} alt='Github'/></a><br/>
                 <a href='https://medium.com/@cortezd334' rel="noreferrer" target='_blank' className='icon'><img src={medium} alt='Medium'/></a><br/>
-                <a href={'mailto:cortezd334@gmail.com'} className='icon'><img src={paper_plane} alt='Email'/></a>
             </div>
         </div>
     )
