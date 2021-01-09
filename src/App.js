0b428b6901ce4a1a51084422812cef522d5c1186
cortezd334 <cortezd334@gmail.com>
2021-01-09 00:55:45 -0800

    for now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Projects from './Components/Projects'
 import Resume from './Components/Resume'
 import Blog from './Components/Blog'
 import Contact from './Components/Contact'
-import test from './images/test.png'
 
 function App() {
 
@@ -61,29 +60,13 @@ function App() {
   // }, [])
 
   return (
-    // <>
-    // {/* comment this section in when updating Under Construction
-    // also have to comment in construction.css in index.css */}
-    // <Router>
-    //   <div id='nav'>
-    //     <NavLink to='/' exact className='inline-block'><img src={process.env.PUBLIC_URL + '/final.png'} alt='home' height='50' width='auto'/></NavLink>
-    //     <h1 className='inline-block marg'>Danira Cortez</h1>
-    //   </div>
-    //   <Switch>
-    //     <Route exact path='/'>
-    //       <Construction/>
-    //     </Route>
-    //   </Switch>
-    // </Router>
-    // </>
-
-
     <>
     <Router>
-    {/* <Router basename={process.env.PUBLIC_URL}> if not using HashRouter */}
+    {/* <Router basename={process.env.PUBLIC_URL}> */}
+       {/* if not using HashRouter */}
       <div id='nav'>
-        <NavLink to='/' exact className='inline-block'><img src={test} alt='home' height='50' width='auto'/></NavLink>
-        {/* <NavLink to='/' exact className='inline-block'><img src={process.env.PUBLIC_URL + '/final.png'} alt='home' height='50' width='auto'/></NavLink> */}
+        {/* <NavLink to='/' exact className='inline-block'><img src={test} alt='home' height='50' width='auto'/></NavLink> */}
+        <NavLink to='/' exact className='inline-block'><img src={process.env.PUBLIC_URL + '/test.png'} alt='home' height='50' width='auto'/></NavLink>
         <h1 className='inline-block marg'>Danira Cortez</h1>
         <NavLink to='/contact' className='tabs'>Contact</NavLink>
         <NavLink to='/blog' className='tabs'>Blog</NavLink>
