98bd8cd6934b46bab6cca160809b832515171fd5
cortezd334 <cortezd334@gmail.com>
2021-01-11 19:31:48 -0800

    resume w/ only email and website

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import resume from '../utils/Resume.pdf'
+import resume from '../utils/EmailWebsiteResume.pdf'
+// import resume from '../utils/Resume.pdf'
 import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -9,7 +10,7 @@ export default function Resume() {
     return(
         <div className='res'>
             <h2 className='heading'>Resume</h2>
-            <a href="https://onedrive.live.com/embed?cid=D3D5958DCD632659&resid=D3D5958DCD632659%2161955&authkey=AJ6AakxKjWLUCIE&em=2" rel="noreferrer" target='_blank'>
+            <a href="https://onedrive.live.com/embed?cid=D3D5958DCD632659&resid=D3D5958DCD632659%2161961&authkey=AD1qLUv7Fz0kDZI&em=2" rel="noreferrer" target='_blank'>
                 <Document
                     file={resume}
                     onLoadError={console.error}
@@ -21,3 +22,4 @@ export default function Resume() {
         </div>
     )
 }
+{/* <iframe src="https://onedrive.live.com/embed?cid=D3D5958DCD632659&resid=D3D5958DCD632659%2161961&authkey=AD1qLUv7Fz0kDZI&em=2" width="476" height="288" frameborder="0" scrolling="no"></iframe> */}
